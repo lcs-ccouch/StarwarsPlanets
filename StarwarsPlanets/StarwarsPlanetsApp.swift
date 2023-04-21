9bb48f9610d1df6bb8f1115282c3be91242a8842
Calum Couch <ccouch@lcs.on.ca>
2023-04-20 22:41:22 -0400

    Initial Commit

---
+++ StarwarsPlanets/StarwarsPlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarwarsPlanetsApp.swift
+//  StarwarsPlanets
+//
+//  Created by calum couch on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct StarwarsPlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
